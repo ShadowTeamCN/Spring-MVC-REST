f2ef9ad7036247daa9edb4b25a0c1d9be1529a78
Liu Dongxiao <linysynro@gmail.com>
2017-02-14 21:25:14 +0800

    Autowired Bug

---
+++ IdeaProjects/mvc/src/main/Java/cn/ldx/test/UserRepositoryTest.java
@@ -0,0 +1,11 @@
+package cn.ldx.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by Doenix Liu on 2017/2/14.
+ */
+public class UserRepositoryTest {
+    @Autowired
+    private  UserRepositoryTest userRepositoryTest;
+}
